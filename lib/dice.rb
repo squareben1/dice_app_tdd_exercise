544997e16af22575f5d4b75e9fdd2a080e9e09f4
Squareben1 <bengittins@live.com>
2020-08-23 22:56:50 +0100

    Sets score of last roll

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,10 +1,23 @@
 class Dice
+  def initialize
+    @roll_history = []
+    @last_score = nil
+  end
 
-  def roll(num=1)
-      arr = []
-      num.times do 
-        arr << rand(1..6)
-      end
-      arr
+  def roll(num = 1)
+    arr = []
+    num.times do
+      arr << rand(1..6)
+    end
+    @last_score = arr.sum
+    @roll_history.concat arr
+  end
+
+  def show_rolls
+    @roll_history
+  end
+
+  def get_score
+    @last_score
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,12 +1,9 @@
 require 'dice'
 
-
-
 describe Dice do
-
   before :each do
     # sets rand to 3
-    srand(67809)
+    srand(67_809)
   end
 
   describe '#roll' do
@@ -17,9 +14,23 @@ describe Dice do
     it 'returns random int (rand set for test)' do
       expect(subject.roll).to eq [3]
     end
-    
+
     it 'roll 2 dice at the same time' do
       expect(subject.roll(2)).to eq [3, 6]
+    end
+  end
+
+  describe '#show_rolls' do
+    it 'returns single roll' do
+      subject.roll
+      expect(subject.show_rolls).to eq [3]
+    end
+  end
+
+  describe '#set_score' do
+    it 'sets score attribute' do
+      subject.roll
+      expect(subject.get_score).to eq 3
     end
   end
 end
